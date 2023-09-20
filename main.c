398b5d60a1144c7efb607ff382b3ab2955dc694c
Ryûga <corentingambier@gmail.com>
2023-09-20 11:01:33 +0200

    cttdx

--- main.c
+++ main.c
@@ -14,6 +14,8 @@ int main() {
 
     printf("Bonjour et Bienvenue dans: le Juste Prix\n");
     printf("Tu peux quitter le jeu à tout moment en entrant 0 ! \n Nous avons généré un nombre entre 1 et 1000,\n Essaie de le deviner !\n");
+// j'ai adapté ma boucle afin que quitter le jeu sois plus ergonomique et moins longue pour l'utilisateur à intégrer en lui ajoutant l'option "quitter le jeu" en entrant 0 à tout moment,
+// de ce fait l'utilisateur n'est gêné à devoir répondre à plusieur questions lors du jeu du style :("voulez-vous continuer oui: 1 ; non : 2") puis ("entrez votre réponse:")
 
 
     while(1){
