ddf9b4eb368ffc0deea8b5277c6f2465f62ffe11
Ryûga <corentingambier@gmail.com>
2023-09-20 09:40:22 +0200

    dm3

---
+++ cmake-build-debug/CMakeFiles/DM3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DM3.dir/main.c.obj"
+  "CMakeFiles/DM3.dir/main.c.obj.d"
+  "DM3.exe"
+  "DM3.exe.manifest"
+  "DM3.pdb"
+  "libDM3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DM3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.c
+++ main.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 int main() {
     srand(time(0));
     int price = (rand() %1000) + 1;
     int userguessing;
     int numberoftry;
+    //type(userguessing == int)
+    //isinstance(userguessing,int)
 
     printf("Bonjour et Bienvenue dans: le Juste Prix\n");
-    printf("Tu peux quitter le jeu à tout moment en entrant 0 ! \n Nous avon généré un nombre entre 1 et 1000,\n Essaie de le deviner !\n");
+    printf("Tu peux quitter le jeu à tout moment en entrant 0 ! \n Nous avons généré un nombre entre 1 et 1000,\n Essaie de le deviner !\n");
 
 
     while(1){
         printf("Enter your number :\n");
         scanf("%d",&userguessing);
+        getchar();
         numberoftry++;
         if(userguessing == 0){
             printf("Aurevoir !");
@@ -24,7 +29,7 @@ int main() {
         }else if(userguessing < price){
             printf("C'est plus!\n");
         }else if (userguessing == price){
-            printf("Bien Joué vous avez Gagné en %d essais! Le prix était : %d\n",numberoftry,price);
+            printf("Bien Joué vous avez Gagné en %d essais! Le prix était : %d euros\n",numberoftry,price);
             break;
         }}
     return 0;
